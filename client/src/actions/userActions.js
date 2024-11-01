6a5b1989c25eca34ec5bf67d6e97e21417604e85
Mercy Mwikali <56218360+mercymwikali@users.noreply.github.com>
2024-11-01 20:31:41 +0300

    Update userActions.js

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -3,7 +3,7 @@ import { USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGIN_FAIL, USER_LOGOUT, U
 
 
 
-const API = "http://localhost:5000/"
+const API = "https://hospital-solution-server.vercel.app/"
 console.log("Base URL: ", API)
 
 export const login = (email, password) => async (dispatch) => {
